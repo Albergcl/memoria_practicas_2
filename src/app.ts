fbadca9575b92d2dd648faf29e93617ec2e7eed6
Alberto <calerolopezalberto@gmail.com>
2026-06-04 16:07:42 +0200

    corrigiendo cosas para que funcione el deploy

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,7 @@ import { swaggerDocumentation } from "./config/swagger";
 
 const app = express();
 
-const allowedOrigins = process.env.FRONTEND_URL
-    ? [process.env.FRONTEND_URL, "http://localhost:3001"]
-    : ["http://localhost:3001"];
-
-app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocumentation));
